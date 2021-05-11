7a9d8084d3c2760f7c3c59ef952e6edce36e9bc4
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-05-11 23:52:43 +0700

    Update Todo feature

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import TodoList from "./components/TodoList";
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-  const todoList = [
+  const initTodoList = [
     {
       id: 1,
       title: "Eat",
+      status: "new",
     },
     {
       id: 2,
       title: "Code",
+      status: "completed",
     },
     {
       id: 3,
       title: "Nap",
+      status: "new",
     },
   ];
+
+  const [todoList, setTodoList] = useState(initTodoList);
+
+  const handleTodoClick = (todo, idx) => {
+    // clone current array to the new array
+    const newTodoList = [...todoList];
+
+    // toggle state
+    newTodoList[idx] = {
+      ...newTodoList[idx],
+      status: newTodoList[idx].status === "new" ? "completed" : "new",
+    };
+
+    //update todo list
+    setTodoList(newTodoList);
+  };
+
   return (
     <div>
       <h3>To Do List</h3>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
