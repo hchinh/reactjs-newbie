893dc37c0851ae5e0b79d51544637f184573028c
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-05-12 22:25:31 +0700

    add filter

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -24,6 +24,7 @@ function TodoFeature(props) {
   ];
 
   const [todoList, setTodoList] = useState(initTodoList);
+  const [filteredStatus, setFilteredStatus] = useState("all");
 
   const handleTodoClick = (idx) => {
     // clone current array to the new array
@@ -39,10 +40,32 @@ function TodoFeature(props) {
     setTodoList(newTodoList);
   };
 
+  const handleShowAllClick = () => {
+    setFilteredStatus("all");
+  };
+
+  const handleShowCompletedClick = () => {
+    setFilteredStatus("completed");
+  };
+
+  const handleShowNewClick = () => {
+    setFilteredStatus("new");
+  };
+
+  const renderedTodoList = todoList.filter(
+    (todo) => filteredStatus === "all" || filteredStatus === todo.status
+  );
+
   return (
     <div>
       <h3>To Do List</h3>
-      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
+      <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+
+      <div>
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowNewClick}>Show New</button>
+      </div>
     </div>
   );
 }
