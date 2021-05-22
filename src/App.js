c02eaf69cba61e19530526cc93efe284d375d679
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-05-22 11:54:20 +0700

    Add Switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Route, Switch } from "react-router";
 import TodoFeature from "./features/Todo";
+import AlbumFeature from "./features/Album";
+import { Link, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
-      {/* <AlbumFeature /> */}
+      <div>Header</div>
+      <p>
+        <NavLink to="/todos" activeClassName="active-menu">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums" activeClassName="active">
+          Album
+        </NavLink>
+      </p>
+
+      <Switch>
+        {/* <Route path="/" component={TodoFeature} /> */}
+        <Route path="/todos" component={TodoFeature} />
+        <Route path="/albums" component={AlbumFeature} />
+      </Switch>
+
+      <div>Footer</div>
     </div>
   );
 }
