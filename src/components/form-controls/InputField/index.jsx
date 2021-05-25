b08efb025d5e8034f4dbd025ae6b37e408475a07
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-05-25 23:18:05 +0700

    Add Todo Form

---
+++ src/components/form-controls/InputField/index.jsx
@@ -0,0 +1,28 @@
+import { TextField } from '@material-ui/core';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Controller } from 'react-hook-form';
+
+InputField.propTypes = {
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+function InputField(props) {
+  const { form, name, label, disabled } = props;
+
+  return (
+    <Controller
+      name={name}
+      control={form.control}
+      render={({ field: { onChange, onBlur, value } }) => (
+        <TextField onChange={onChange} onBlur={onBlur} selected={value} label={label} disabled={disabled} fullWidth />
+      )}
+    />
+  );
+}
+
+export default InputField;
--- src/features/Todo/pages/ListPage/index.jsx
+++ src/features/Todo/pages/ListPage/index.jsx
@@ -1,7 +1,8 @@
-import queryString from "query-string";
-import React, { useEffect, useMemo, useState } from "react";
-import { useHistory, useLocation, useRouteMatch } from "react-router";
-import TodoList from "../../components/TodoList";
+import queryString from 'query-string';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useHistory, useLocation, useRouteMatch } from 'react-router';
+import TodoForm from '../../components/TodoForm';
+import TodoList from '../../components/TodoList';
 
 ListPage.propTypes = {};
 
@@ -9,18 +10,18 @@ function ListPage(props) {
   const initTodoList = [
     {
       id: 1,
-      title: "Eat",
-      status: "new",
+      title: 'Eat',
+      status: 'new',
     },
     {
       id: 2,
-      title: "Code",
-      status: "completed",
+      title: 'Code',
+      status: 'completed',
     },
     {
       id: 3,
-      title: "Nap",
-      status: "new",
+      title: 'Nap',
+      status: 'new',
     },
   ];
 
@@ -31,12 +32,12 @@ function ListPage(props) {
   const [todoList, setTodoList] = useState(initTodoList);
   const [filteredStatus, setFilteredStatus] = useState(() => {
     const params = queryString.parse(location.search);
-    return params.status || "all";
+    return params.status || 'all';
   });
 
   useEffect(() => {
     const params = queryString.parse(location.search);
-    return setFilteredStatus(params.status || "all");
+    return setFilteredStatus(params.status || 'all');
   }, [location.search]);
 
   const handleTodoClick = (idx) => {
@@ -46,7 +47,7 @@ function ListPage(props) {
     // toggle state
     newTodoList[idx] = {
       ...newTodoList[idx],
-      status: newTodoList[idx].status === "new" ? "completed" : "new",
+      status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
     };
 
     //update todo list
@@ -55,7 +56,7 @@ function ListPage(props) {
 
   const handleShowAllClick = () => {
     // setFilteredStatus("all");
-    const queryParams = { status: "all" };
+    const queryParams = { status: 'all' };
     history.push({
       pathname: match.path,
       search: queryString.stringify(queryParams),
@@ -64,7 +65,7 @@ function ListPage(props) {
 
   const handleShowCompletedClick = () => {
     // setFilteredStatus("completed");
-    const queryParams = { status: "completed" };
+    const queryParams = { status: 'completed' };
     history.push({
       pathname: match.path,
       search: queryString.stringify(queryParams),
@@ -73,7 +74,7 @@ function ListPage(props) {
 
   const handleShowNewClick = () => {
     // setFilteredStatus("new");
-    const queryParams = { status: "new" };
+    const queryParams = { status: 'new' };
     history.push({
       pathname: match.path,
       search: queryString.stringify(queryParams),
@@ -81,13 +82,18 @@ function ListPage(props) {
   };
 
   const renderedTodoList = useMemo(() => {
-    return todoList.filter(
-      (todo) => filteredStatus === "all" || filteredStatus === todo.status
-    );
+    return todoList.filter((todo) => filteredStatus === 'all' || filteredStatus === todo.status);
   }, [todoList, filteredStatus]);
+
+  const handleTodoFormSubmit = (values) => {
+    console.log(values);
+  };
 
   return (
     <div>
+      <h3>What to do</h3>
+      <TodoForm onSubmit={handleTodoFormSubmit} />
+
       <h3>To Do List</h3>
       <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
 
