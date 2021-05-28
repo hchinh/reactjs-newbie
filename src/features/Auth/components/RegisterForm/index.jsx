f01165ed008ecdc2bc5e76dfa7d6642fea36297a
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-05-28 11:00:39 +0700

    Create password field

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -1,20 +1,41 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, Button, makeStyles, Typography } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 import InputField from 'components/form-controls/InputField';
+import PasswordField from 'components/form-controls/PasswordField';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(4),
+  },
+
+  avatar: {
+    margin: '0 auto',
+    backgroundColor: theme.palette.secondary.main,
+  },
+
+  title: {
+    margin: theme.spacing(2, 0, 3, 0),
+    textAlign: 'center',
+  },
+
+  submit: {
+    margin: theme.spacing(3, 0, 2, 0),
+  },
+}));
 
 RegisterForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 function RegisterForm(props) {
-  const schema = yup.object().shape({
-    title: yup.string().required('Please enter title').min(5, 'Title is too short'),
-  });
+  const classes = useStyle();
+
+  const schema = yup.object().shape({});
 
   const form = useForm({
     defaultValues: {
@@ -35,20 +56,24 @@ function RegisterForm(props) {
   };
 
   return (
-    <div>
-      <Avatar>
+    <div className={classes.root}>
+      <Avatar className={classes.avatar}>
         <LockOutlined></LockOutlined>
       </Avatar>
 
-      <Typography component="h3" variant="h5">
+      <Typography className={classes.title} component="h3" variant="h5">
         Create An Account
       </Typography>
 
       <form onSubmit={form.handleSubmit(handleSubmit)}>
         <InputField name="fullName" label="Full Name" form={form} />
         <InputField name="email" label="Email" form={form} />
-        <InputField name="password" label="Password" form={form} />
-        <InputField name="retypePassword" label="Retype Password" form={form} />
+        <PasswordField name="password" label="Password" form={form} />
+        <PasswordField name="retypePassword" label="Retype Password" form={form} />
+
+        <Button type="submit" className={classes.submit} variant="contained" color="primary" fullWidth>
+          Create an account
+        </Button>
       </form>
     </div>
   );
