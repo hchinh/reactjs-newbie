38573d2fa6dfd44dc069b70c45e12c3187253a1c
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-06-05 12:55:07 +0700

    Show filter viewer

---
+++ src/features/Product/components/Filters/FilterViewer.jsx
@@ -0,0 +1,120 @@
+import { Box, Chip, makeStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React, { useMemo } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    alignItems: 'center',
+
+    padding: 0,
+    margin: theme.spacing(2, 0),
+    listStyleType: 'none',
+
+    '& > li': {
+      margin: 0,
+      padding: theme.spacing(1),
+    },
+  },
+}));
+
+const FILTER_LIST = [
+  {
+    id: 1,
+    getLabel: () => 'Giao hàng miễn phí',
+    isActive: (filters) => filters.isFreeShip,
+    isVisible: () => true,
+    isRemovable: false,
+    onRemove: () => {},
+    onToggle: (filters) => {
+      const newFilters = { ...filters };
+      if (newFilters.isFreeShip) {
+        delete newFilters.isFreeShip;
+      } else {
+        newFilters.isFreeShip = true;
+      }
+
+      return newFilters;
+    },
+  },
+
+  {
+    id: 2,
+    getLabel: () => 'Có khuyến mãi',
+    isActive: () => true,
+    isVisible: (filters) => filters.isPromotion,
+    isRemovable: true,
+    onRemove: (filters) => {
+      const newFilters = { ...filters };
+      delete newFilters.isPromotion;
+      return newFilters;
+    },
+    onToggle: () => {},
+  },
+
+  {
+    id: 3,
+    getLabel: (filters) => `Từ ${filters.salePrice_gte} đến ${filters.salePrice_lte}`,
+    isActive: () => true,
+    isVisible: (filters) => filters.salePrice_gte > 0 && filters.salePrice_lte > 0,
+    isRemovable: true,
+    onRemove: (filters) => {
+      const newFilters = { ...filters };
+      delete newFilters.salePrice_lte;
+      delete newFilters.salePrice_gte;
+      return newFilters;
+    },
+    onToggle: () => {},
+  },
+];
+
+FilterViewer.propTypes = {
+  filters: PropTypes.object,
+  onChange: PropTypes.func,
+};
+
+function FilterViewer({ filters = {}, onChange = null }) {
+  const classes = useStyles();
+
+  const visibleFilters = useMemo(() => {
+    return FILTER_LIST.filter((x) => x.isVisible(filters));
+  }, [filters]);
+
+  return (
+    <Box component="ul" className={classes.root}>
+      {visibleFilters.map((x) => (
+        <li key={x.id}>
+          <Chip
+            label={x.getLabel(filters)}
+            color={x.isActive(filters) ? 'primary' : 'default'}
+            size="small"
+            clickable={!x.isRemovable}
+            onClick={
+              x.isRemovable
+                ? null
+                : () => {
+                    if (!onChange) return;
+
+                    const newFilters = x.onToggle(filters);
+                    onChange(newFilters);
+                  }
+            }
+            onDelete={
+              x.isRemovable
+                ? () => {
+                    if (!onChange) return;
+
+                    const newFilters = x.onRemove(filters);
+                    onChange(newFilters);
+                  }
+                : null
+            }
+          />
+        </li>
+      ))}
+    </Box>
+  );
+}
+
+export default FilterViewer;
